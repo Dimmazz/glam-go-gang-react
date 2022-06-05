f32f6bf3cb2e78681567bdacc96926af60b70dde
Dimmazz <zaskok20@yandex.ru>
2022-06-05 05:08:19 +0300

    [Update Component] End Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,8 +5,17 @@ export default function Footer(params) {
     return (
         <footer className="footer">
             <div className="footer__copy">
-
+                <div className="footer__title">
+                    &#169; 2020 Glam Go! 
+                </div>
+                <div className="footer__creator-tag">
+                    Desing & codding - <a className="footer__link" href="https://t.me/MazuSad">MazuSad</a>
+                </div>
+                <div className="footer__inscription">
+                    All Right Reserved.
+                </div>
             </div>
+
             <ul className="footer__media">
                 <li></li>
                 <li></li>
