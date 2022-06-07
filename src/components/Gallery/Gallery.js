cd722b75fd38c626295f08ffb401ecb5a046471a
Dimmazz <zaskok20@yandex.ru>
2022-06-07 13:20:36 +0300

    [Add Component] Add Gallery

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,11 +1,14 @@
 import React from "react";
+import galleryImg from "../Home/img/sbg.jpg"
+import galleryImg2 from "../Music/img/cover.jpeg"
 import "./Gallery.scss"
 
 export default function Gallery(props) {
     return (
         <section className="gallery">
             <ul className="gallery__list">
-                
+                <li className="list__item"><img className="item__img" src={galleryImg} alt="" /></li>
+                <li className="list__item"><img className="item__img" src={galleryImg2} alt="" /></li>
             </ul>
         </section>
     );
