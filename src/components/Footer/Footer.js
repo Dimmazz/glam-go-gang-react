565c45525bc5c3c5df00f7d05ca8b886e90e2974
Dimmazz <zaskok20@yandex.ru>
2022-06-10 11:46:51 +0300

    [UpdateComponent] Update Footer (add dynamic year)

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,11 +2,17 @@ import "./Footer.scss";
 import React from "react";
 
 export default function Footer(params) {
+
+  const getYear = () => {
+    let time = new Date()
+    return time.getFullYear()
+  }
+
   return (
     <footer className="footer container">
       <div className="footer__copy">
         <div className="footer__title">
-          &#169; 2020 Glam Go!
+          &#169; 2018 &ndash; {getYear()} Glam Go!
         </div>
         <div className="footer__creator-tag">
           Desing & codding - <a className="footer__link" href="https://t.me/MazuSad">MazuSad</a>
